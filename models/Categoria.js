b139f20a4bd434b4fc0153c562b2a630f669d672
juanchi26 <juanchi.diaz@hotmail.com>
2023-12-13 17:07:57 -0300

    .
    .

--- models/Categoria.js
+++ models/Categoria.js
@@ -3,7 +3,7 @@ import { DataTypes } from "sequelize"
 import db from "../config/db.js"
 
 
-const Categoria = db.define('Categorias', {
+const Categoria = db.define('categorias', {
         nombre:{
             type: DataTypes.STRING(30),
             allowNull: false
--- models/Precio.js
+++ models/Precio.js
@@ -3,7 +3,7 @@ import { DataTypes } from "sequelize"
 import db from "../config/db.js"
 
 
-const Precio = db.define('Precios', {
+const Precio = db.define('precios', {
         precio:{
             type: DataTypes.STRING(30),
             allowNull: false
